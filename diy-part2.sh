f9092febe7ad00a39ef92680385e780004e0e27c
liq <64891712+wosli@users.noreply.github.com>
2024-02-19 23:07:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git pull
 ./scripts/feeds update -a && ./scripts/feeds install -a
