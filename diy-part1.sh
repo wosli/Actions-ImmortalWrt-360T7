99c14826df412081c02921b48c6d20731fb13c27
liq <64891712+wosli@users.noreply.github.com>
2024-02-19 22:53:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,17 +21,12 @@ sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 - name: 替换默认主题 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
-# 修改时区 UTF-8
-sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
-
-# 时区
-sed -i 's/time1.apple.com/time1.cloud.tencent.com/g'  package/base-files/files/bin/config_generate
-sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/config_generate
-sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
-sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
-
 # 替换源 
 sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
+
+# Update Go to 1.21 for Xray-core build
+rm -rf feeds/packages/lang/golang
+git_sparse_clone https://github.com/immortalwrt/packages openwrt-23.05 lang/golang feeds/packages/lang/golang
 
 # Make tailsale config persistent during sysupgrades
 echo "/etc/tailscale/" >> package/base-files/files/etc/sysupgrade.conf
