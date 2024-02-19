c037ac32ea2271b23693fd686721e7ea2e50fdc8
liq <64891712+wosli@users.noreply.github.com>
2024-02-20 01:01:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,4 +10,18 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# Clean up dependencies
+find feeds -name Makefile -exec dirname {} \; | grep -wE 'brook|gn|chinadns-ng|dns2socks|dns2tcp|hysteria|ipt2socks|microsocks|naiveproxy|pdnsd-alt|redsocks2|sagernet-core|shadowsocks-rust|shadowsocksr-libev|simple-obfs|sing-box|ssocks|tcping|trojan|trojan-go|trojan-plus|v2ray-core|v2ray-geodata|v2ray-plugin|v2raya|xray-core|xray-plugin|lua-neturl|luci-app-ssr-plus|mosdns' | xargs rm -rf
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Modify default Hostname
+#sed -i 's/ImmortalWrt/Railgun/g' package/base-files/files/bin/config_generate
+
+- name: 替换默认主题 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+# Update Go to 1.21 for Xray-core build
+rm -rf feeds/packages/lang/golang
+git_sparse_clone https://github.com/immortalwrt/packages openwrt-23.05 lang/golang feeds/packages/lang/golang
