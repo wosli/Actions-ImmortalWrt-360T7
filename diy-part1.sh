69e658bd00a77b616b38cc73f6f8e959a3cb1c07
liq <64891712+wosli@users.noreply.github.com>
2024-02-20 13:22:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,4 +11,7 @@
 #
 
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall
