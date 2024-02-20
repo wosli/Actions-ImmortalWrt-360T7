e673b3797acc1145ddb6e6b3a85dfb7c31e116b9
liq <64891712+wosli@users.noreply.github.com>
2024-02-20 21:24:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,29 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Clean up dependencies
-find feeds -name Makefile -exec dirname {} \; | grep -wE 'brook|gn|chinadns-ng|dns2socks|dns2tcp|hysteria|ipt2socks|microsocks|naiveproxy|pdnsd-alt|redsocks2|sagernet-core|shadowsocks-rust|shadowsocksr-libev|simple-obfs|sing-box|ssocks|tcping|trojan|trojan-go|trojan-plus|v2ray-core|v2ray-geodata|v2ray-plugin|v2raya|xray-core|xray-plugin|lua-neturl|luci-app-ssr-plus|mosdns' | xargs rm -rf
 
-# Modify default IP
-sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-# Modify default Hostname
-#sed -i 's/ImmortalWrt/Railgun/g' package/base-files/files/bin/config_generate
-
-- name: 替换默认主题 luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-
-# 修改时区 UTF-8
-sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
-
-# 时区
-sed -i 's/time1.apple.com/time1.cloud.tencent.com/g'  package/base-files/files/bin/config_generate
-sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/config_generate
-sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
-sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
-
-# 替换源 
-sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
-
-# Make tailsale config persistent during sysupgrades
-echo "/etc/tailscale/" >> package/base-files/files/etc/sysupgrade.conf
+# passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall
