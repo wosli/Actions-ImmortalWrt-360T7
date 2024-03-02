4eb87fda277a3b6d5bbab0c1f8b77ba7f85c0271
li <1111111111111@1.com>
2024-03-02 23:07:52 +0800

    Revert "updata"
    This reverts commit 5359f5c1699730a387919bcca282586ef859b121.

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-
-
